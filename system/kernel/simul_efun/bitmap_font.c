6b4c7e2f303318e2ee7bdf8941e28e0e179b69f5
oiuv <i@oiuv.cn>
2020-03-05 17:03:07 +0800

    update bitmap_font sefun

--- system/kernel/simul_efun/bitmap_font.c
+++ system/kernel/simul_efun/bitmap_font.c
@@ -21,7 +21,7 @@ varargs string bitmap_font(string str, int size, string fill, string bg, string 
     int offset, fontsize, scale;
     int *mask = ({0x80, 0x40, 0x20, 0x10, 0x8, 0x4, 0x2, 0x1});
     buffer char, bstr = string_encode(str, "GBK");
-    string *out;
+    string file, *out;
     // 当前可用字库16x12、16x14、16x16
     if (member_array(size, ({12, 14, 16})) < 0)
         size = AUTO_SIZE;
@@ -45,6 +45,11 @@ varargs string bitmap_font(string str, int size, string fill, string bg, string 
 
     if (!fcolor) fcolor = DEFAULT_FCOLOR;
     if (!bgcolor) bgcolor = DEFAULT_BGCOLOR;
+    // 16x16中文字库随机字体
+    if (size == 16)
+        file = HZK + size + element_of(({"", "F", "H", "K", "L", "S", "V", "X", "Y"}));
+    else
+        file = HZK + size;
 
     for (int k = 0; k < sizeof(bstr); k++)
     {
@@ -52,9 +57,9 @@ varargs string bitmap_font(string str, int size, string fill, string bg, string 
         {
             // 区码：汉字的第一个字节-0xA0
             // 位码：汉字的第二个字节-0xA0
-            // offset = (94*(区码-1)+(位码-1))* fontsize;
+            // offset = (94 * (区码 - 1) + (位码 - 1)) * fontsize;
             offset = fontsize * ((bstr[k] - 0xA1) * 94 + bstr[k+1] - 0xA1);
-            char = read_buffer(HZK + size, offset, fontsize);
+            char = read_buffer(file, offset, fontsize);
             k++;
         } else {
             // 英文每个字符占1字节
@@ -63,7 +68,8 @@ varargs string bitmap_font(string str, int size, string fill, string bg, string 
             scale = 1;
         }
 
-        if (!sizeof(char)) error("Can't read bytes from character lib\n");
+        if (!sizeof(char)) return "Can't read bytes from character lib.\n";
+
         //填充字符
         for (int i = 0; i < sizeof(char); i++)
         {
@@ -82,5 +88,6 @@ varargs string bitmap_font(string str, int size, string fill, string bg, string 
             out[i] = replace_string(out[i], bg + bgcolor + bg, bg + bg);
         }
     }
+
     return implode(out, "\n");
 }
