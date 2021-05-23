f9b6eea7d6ebb42acb2e854f77e220390c1018fa
oiuv <i@oiuv.cn>
2021-05-23 14:39:25 +0800

    美化错误日志格式

--- system/kernel/master/error.c
+++ system/kernel/master/error.c
@@ -1,5 +1,9 @@
 // error.c
 #include <runtime_config.h>
+
+#define TRACE_DETAIL_LENGTH_LIMIT   300
+string tracert_error(mapping error, int caught);
+
 /**
  * @brief 日志记录
  *
@@ -35,17 +39,9 @@ void log_error(string file, string message)
  */
 void error_handler(mapping map, int flag)
 {
-    string str = "[" + ctime() + "]";
-
-    str += sprintf("\nError: %s\nProgram: %s\nObject: %O\nFile: %s - Line: %d\n[%s]\n",
-                   map["error"], (map["program"] || "No program"),
-                   (map["object"] || "No object"),
-                   map["file"],
-                   map["line"],
-                   implode(map_array(map["trace"],
-                                        (: sprintf("\n\tProgram: %s\n\tObject: %O \n\tFile: %s\n\tFunction : %s\n\tLine: %d\n", $1["program"], $1["object"], $1["file"], $1["function"], $1["line"]) :)
-                                    ), "\n"));
-    str += sprintf("%O\n\n", map);
+    string str = tracert_error(map, flag);
+    // 附加原始信息
+    str += sprintf("%O\n", map);
 
     if (flag)
         printf("%s", map["error"]);
@@ -53,4 +49,57 @@ void error_handler(mapping map, int flag)
         debug("出错啦！详情记录在 error_handler 日志。");
 
     log_file("error_handler", str);
+}
+
+/* 处理错误讯息的函数 */
+string tracert_error(mapping error, int caught)
+{
+    int count;
+    string err_msg;
+    mapping trace;
+
+    err_msg = sprintf(@ERR
+──────────────<Bugs Report>──────────────
+[ 错误时间 ]: %-s
+[ 错误內容 ]: %-s
+[ 错误档案 ]: %-s
+[ 错误行数 ]: %-d
+[ 资料回溯 ]:
+ERR,
+    ctime(time()),
+    replace_string(error["error"], "\n", " "),
+    error["file"],
+    error["line"]);
+
+    foreach(trace in error["trace"])
+    {
+        count++;
+        err_msg += sprintf(@ERR
+    -- 第 %|3d 笔 --
+        [ 触发对象 ]: %O
+        [ 程序档案 ]: %s
+        [ 函数名称 ]: %s(%s)
+        [ 呼叫行数 ]: %s
+ERR,
+        count,
+        trace["object"],
+        trace["program"] || "",
+        trace["function"] || "",
+        trace["arguments"] ? implode(map(trace["arguments"], (: typeof($1) :)), ", ") : "",
+        (trace["line"] || "未知") + "");
+
+        if (trace["arguments"])
+        {
+            err_msg += "        [ 传入参数 ]:\n";
+            err_msg += implode(map(trace["arguments"], (: "                   ** (" + typeof($1) + ")" + implode(explode(sprintf("%." + TRACE_DETAIL_LENGTH_LIMIT + "O\n", $1) + (strlen(sprintf("%O", $1)) > TRACE_DETAIL_LENGTH_LIMIT ? "... 讯息过长省略\n" : ""), "\n"), "\n                      ") :)), "\n") + "\n";
+        }
+        if (trace["locals"])
+        {
+            err_msg += "        [ 程序变量 ]:\n";
+            err_msg += implode(map(trace["locals"], (: "                   ** (" + typeof($1) + ")" + implode(explode(sprintf("%." + TRACE_DETAIL_LENGTH_LIMIT + "O\n", $1) + (strlen(sprintf("%O", $1)) > TRACE_DETAIL_LENGTH_LIMIT ? "... 讯息过长省略\n" : ""), "\n"), "\n                      ") :)), "\n") + "\n";
+        }
+    }
+    err_msg += "──────────────<Bugs Report>──────────────\n";
+
+    return err_msg;
 }
